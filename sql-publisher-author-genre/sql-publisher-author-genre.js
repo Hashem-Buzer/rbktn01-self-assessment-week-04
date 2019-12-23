34572bcce0732d6ba508099e958ffb6e539e33f9
henimezrani <henimezrani@gmail.com>
2019-12-23 09:51:09 +0100

    weekly assessment solved

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -9,11 +9,21 @@ var Schemas = {
   OPTION_6: 'option 6'
 };
 
-var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+var sqlPublisherAuthorSchema =  Schemas.OPTION_6; // TODO: Update this constant
+// A publisher can have multiple authors
+// An author can have multiple publisher
+// This is a Many-to-Many relatioship meaning that it needs to be modeled
+// Using a join table containing the reference IDs of each author and publisher
 
-var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+var sqlPublisherGenreSchema =  Schemas.OPTION_4; // TODO: Update this constant
+// A genre can have multiple publishers
+// A publisher can only have one genre
+// This is a One-to-Many relationship meaning that the the table that can only
+// take one value (In our case "publisher") needs to have a foreign key referencing
+// the other table's primary key (In our case, the id_genre)
 
-var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+var sqlAuthorGenreSchema =  Schemas.OPTION_6; // TODO: Update this constant
+// A genre can have multiple authors
+// An author can have multiple genres
+// This is a Many-to-Many relatioship meaning that it needs to be modeled
+// Using a join table containing the reference IDs of each author and genre
