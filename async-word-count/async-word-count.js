7d94a6e4e5c0da665d32dd260632f4140823f1d5
fatima mabrouk <onlyfefe03@gmail.com>
2019-12-23 10:01:15 +0100

    assessment

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -2,19 +2,38 @@ var fs = require('fs');
 var path = require('path');
 
 var getWordCount = function(filePath, callback) {
-  fs.readFile(filePath, 'utf-8', function(err, data) {
-    if (err) {
-      callback(err, null);
-      return;
-    }
+    fs.readFile(filePath, 'utf-8', function(err, data) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
 
-    var wordCount = data.trim().split(' ').length;
-    callback(null, wordCount);
-  });
+        var wordCount = data.trim().split(' ').length;
+        callback(null, wordCount);
+    });
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+    // YOUR CODE HERE
+
+    getWordCount(filePathOne, (err, fileDate1) => {
+        if (err) {
+            callback(err, null);
+        }
+        var wordcoun1 = fileDate1;
+        callback(null, wordcoun1);
+        // should return how many word do we have then we call it for the pathe to and save the countfileword one wuth two 
+        var wordcount2 = fileDate2;
+        getWordCount(filePathTwo, (err, fileDate2) => {
+            if (err) {
+                callback(null, err);
+            }
+            callback(null, wordcoun1 + wordcount2);
+        })
+    })
+
+
+
 };
 
 module.exports = getTotalWordCount;
--- node-req-count/server.js
+++ node-req-count/server.js
@@ -5,17 +5,17 @@ var path = require('path');
 var globalCounter = {};
 
 var server = http.createServer(function(request, response) {
-  var endpoint = url.parse(request.url, true).pathname;
-  var property = endpoint.replace(/^\//, '');
+    var endpoint = url.parse(request.url, true).pathname;
+    var property = endpoint.replace(/^\//, '');
 
-  if (request.method === 'POST') {
-    // YOUR CODE HERE
-  } else if (request.method === 'GET') {
-    // YOUR CODE HERE
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
+    if (request.method === 'POST') {
+        // response.
+    } else if (request.method === 'GET') {
+        request.end(globalCounter[property]); // this will git the last globalecounter.dog or ..
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
 });
 
 // Do not edit this line
