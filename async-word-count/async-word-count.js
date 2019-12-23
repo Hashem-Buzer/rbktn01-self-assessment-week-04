e38c44cafb9b564857442ef106d09b9ad461ddc3
amineslaimia <medamineslaimia@gmail.com>
2019-12-23 10:04:06 +0100

    done

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 
+
 var getWordCount = function(filePath, callback) {
   fs.readFile(filePath, 'utf-8', function(err, data) {
     if (err) {
@@ -15,6 +16,30 @@ var getWordCount = function(filePath, callback) {
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+filePathOne = path.join(__dirname,'files/fileOne.txt');
+filePathOne = path.join(__dirname,'files/fileTow.txt');
+  var result =0
+  getWordCount(filePathOne ,(err,res1)=>{
+     if(err){
+       return err
+     }else{
+        result= result +res1
+        getWordCount(filePathTwo,(err,res2)=>{
+          if(err){
+            return err
+
+          }else{
+            result= result +res2
+            callback(result)
+          }
+        })
+     }
+  })
+    
+
+    
+  
+    
 };
 
 module.exports = getTotalWordCount;
