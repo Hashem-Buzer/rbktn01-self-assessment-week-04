52d8a0ec8a3c5de21f4c3bf5a5f9725f0955d2a5
rue-elwahishi <rubawahishi@gmail.com>
2019-12-23 10:08:01 +0100

    assessment week 4

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -1,20 +1,33 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 var getWordCount = function(filePath, callback) {
-  fs.readFile(filePath, 'utf-8', function(err, data) {
+  fs.readFile(filePath, "utf-8", function(err, data) {
     if (err) {
       callback(err, null);
       return;
     }
 
-    var wordCount = data.trim().split(' ').length;
+    var wordCount = data.trim().split(" ").length;
     callback(null, wordCount);
   });
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+  fs.readFile(filePathOne, function(err, data) {
+    if (err) {
+      return;
+    }
+    var wordCountForFileOne = data.trim().split(" ").length;
+    fs.readFile(filePathTwo, function(err, date) {
+      if (err) {
+        return;
+      }
+      var wordCountForFileTwo = data.trim().split(" ").length;
+    });
+  });
+  var totalWordCount = wordCountForFileOne + wordCountForFileTwo;
+  callback(null, wordCount);
 };
 
 module.exports = getTotalWordCount;
--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -1,19 +1,23 @@
 var Schemas = {
-  FIX_ME: 'wrong answer',
-  OPTION_0: 'option 0',
-  OPTION_1: 'option 1',
-  OPTION_2: 'option 2',
-  OPTION_3: 'option 3',
-  OPTION_4: 'option 4',
-  OPTION_5: 'option 5',
-  OPTION_6: 'option 6'
+  FIX_ME: "wrong answer",
+  OPTION_0: "option 0",
+  OPTION_1: "option 1",
+  OPTION_2: "option 2",
+  OPTION_3: "option 3",
+  OPTION_4: "option 4",
+  OPTION_5: "option 5",
+  OPTION_6: "option 6"
 };
 
-var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlPublisherAuthorSchema = Schemas.OPTION_6; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// many to many relationship needs join table
 
-var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlPublisherGenreSchema = Schemas.OPTION_6; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// many to many relationships requires join table
 
-var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlAuthorGenreSchema = Schemas.OPTION_3; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// one to many relationships a genre has many authors while an author has only one genre
+// when we have a one to many relationship the 'one' id goes to the 'many' table.
