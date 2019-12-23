f483570d270d33a64c19b3ac7b09d28efbe70197
mfmehdi <mfmehdi2@gmail.com>
2019-12-23 09:46:52 +0100

    done

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -1,17 +1,27 @@
-var url = require('url');
-var http = require('http');
-var path = require('path');
+var url = require("url");
+var http = require("http");
+var path = require("path");
 
 var globalCounter = {};
 
 var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
-  var property = endpoint.replace(/^\//, '');
+  var property = endpoint.replace(/^\//, "");
 
-  if (request.method === 'POST') {
+  if (request.method === "POST") {
     // YOUR CODE HERE
-  } else if (request.method === 'GET') {
+    if (globalCounter.dog === undefined) {
+      globalCounter.dog = 1;
+    } else {
+      globalCounter.dog = globalCounter.dog + 1;
+    }
+    response.statusCode = 201;
+    response.end();
+  } else if (request.method === "GET") {
     // YOUR CODE HERE
+    response.write(globalCounter.dog);
+    response.statusCode = 200;
+    response.end();
   } else {
     response.statusCode = 404;
     response.end();
