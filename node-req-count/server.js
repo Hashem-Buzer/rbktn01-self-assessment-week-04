bda0ad17f58b092e7b915a7df847946031462aa3
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-12-23 09:59:40 +0100

    done

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -2,16 +2,23 @@ var url = require('url');
 var http = require('http');
 var path = require('path');
 
-var globalCounter = {};
+var globalCounter = {
 
-var server = http.createServer(function(request, response) {
+};
+
+var server = http.createServer(function (request, response) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
 
   if (request.method === 'POST') {
     // YOUR CODE HERE
+    if (globalCounter[endpoint] === undefined) {
+      globalCounter[endpoint] = 1;
+    }
+    else globalCounter[endpoint]++
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
+    return globalCounter[endpoint];
   } else {
     response.statusCode = 404;
     response.end();
