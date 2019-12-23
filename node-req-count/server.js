4328e49bc450dd7c93fe1a82176fe8fcbf165805
SofianSaleh <sofiane.saleh11@gmail.com>
2019-12-23 09:38:01 +0100

    2

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -4,29 +4,30 @@ var path = require('path');
 
 var globalCounter = {};
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
   var endpoint = url.parse(request.url, true).pathname;
-  var prop = endpoint.replace(/^\//, '');
+  // Replaces any endpoint('/index) to ('index') it removes the forward slash
+  var property = endpoint.replace(/^\//, '');
 
   if (request.method === 'POST') {
     // YOUR CODE HERE
     // Check if the property exists if it doesn't create it.
-    if(!globalCounter[prop]) {
-      globalCounter[prop] = 0;
+    if (!globalCounter[property]) {
+      globalCounter[property] = 0;
     }
-    // Increment the property
-    globalCounter[prop]++;
+    // Increment the propertyerty
+    globalCounter[property]++;
     // For test purposes
-    console.log(globalCounter[prop])
+    console.log(globalCounter[property])
 
-    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.writeHead(200, { 'Content-Type': 'application/json' })
     response.end()
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
     // For test purposes
-    console.log(globalCounter[prop])
-    
-    response.writeHead(200, {'Content-Type': 'application/json'})
+    console.log(globalCounter[property])
+
+    response.writeHead(200, { 'Content-Type': 'application/json' })
     response.end()
   } else {
     response.statusCode = 404;
