a03c0601833c4fe0759261ac5a61b4d78be52037
SofianSaleh <sofiane.saleh11@gmail.com>
2019-12-23 09:27:52 +0100

    1

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -15,6 +15,23 @@ var getWordCount = function(filePath, callback) {
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+  // Pass the first file to the getWordCount function to count it.
+  getWordCount(filePathOne, (err, data) => {
+    // Check if there is an error.
+    if(err) {
+      callback(err, null)
+    }else {
+      // If there are no errors then move on to the second file.
+      getWordCount(filePathTwo, (err, data) => {
+        if(err) {
+          callback(err, null)
+        }else {
+          //Apply the callback to the result.
+          callback(null, data)
+        }
+      })
+    }
+  })
 };
 
 module.exports = getTotalWordCount;
--- node-req-count/server.js
+++ node-req-count/server.js
@@ -6,12 +6,28 @@ var globalCounter = {};
 
 var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
-  var property = endpoint.replace(/^\//, '');
+  var prop = endpoint.replace(/^\//, '');
 
   if (request.method === 'POST') {
     // YOUR CODE HERE
+    // Check if the property exists if it doesn't create it.
+    if(!globalCounter[prop]) {
+      globalCounter[prop] = 0;
+    }
+    // Increment the property
+    globalCounter[prop]++;
+    // For test purposes
+    console.log(globalCounter[prop])
+
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end()
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
+    // For test purposes
+    console.log(globalCounter[prop])
+    
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end()
   } else {
     response.statusCode = 404;
     response.end();
