da6f353a6b98071ca0a9b6c11377477cbb68bb35
rue-elwahishi <rubawahishi@gmail.com>
2019-12-23 11:28:03 +0100

    Week 4 assessment

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -13,11 +13,10 @@ var sqlPublisherAuthorSchema = Schemas.OPTION_6; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
 // many to many relationship needs join table
 
-var sqlPublisherGenreSchema = Schemas.OPTION_6; // TODO: Update this constant
+var sqlPublisherGenreSchema = Schemas.OPTION_4; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
-// many to many relationships requires join table
+// one to many relationship, you take the id from the table that has many publishers, to the publisher table that has only one genre;
 
-var sqlAuthorGenreSchema = Schemas.OPTION_3; // TODO: Update this constant
+var sqlAuthorGenreSchema = Schemas.OPTION_6; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
-// one to many relationships a genre has many authors while an author has only one genre
-// when we have a one to many relationship the 'one' id goes to the 'many' table.
+//many to many relationship
