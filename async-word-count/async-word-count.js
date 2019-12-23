1089ede9fb6cbf4f9c667c1a3bf49858c59b226a
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-23 09:11:55 +0100

    async word count complete

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -14,7 +14,33 @@ var getWordCount = function(filePath, callback) {
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+  fs.readFile(filePathOne, 'utf-8', (err, content1) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    fs.readFile(filePathTwo, 'utf-8', (err, content2) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      // Join the two files content together and add space between two paragraphs
+      callback(null, (content1 + ' '+ content2).trim().split(' ').length);
+    });
+  });
 };
+
+
+let filePathOne = path.join(__dirname, 'files', 'fileOne.txt');
+let filePathTwo = path.join(__dirname, 'files', 'fileTwo.txt');
+
+getTotalWordCount(filePathOne, filePathTwo, (err, data)=>{
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
 
 module.exports = getTotalWordCount;
